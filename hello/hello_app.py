bfd8a3dab49999224ffd260a6444f463ec9c0338
fusionx1 <paul.depaula@gmail.com>
2016-07-12 18:47:43 +0800

    updated app

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>ello World, I love continuous delivery a lot!</h1>"
+    return "<h1>Hello World, I love continuous delivery a lot!</h1>"
 
 if __name__ == "__main__":
     app.run()
