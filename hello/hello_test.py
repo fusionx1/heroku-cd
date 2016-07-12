2c4899a39ed32eaf54c11bc7956d3f73e3de63ac
fusionx1 <paul.depaula@gmail.com>
2016-07-12 18:22:13 +0800

    updated test

--- hello/hello_test.py
+++ hello/hello_test.py
@@ -9,4 +9,4 @@ class HelloTest(unittest.TestCase):
         self.app = app.test_client()
 
     def test_root(self):
-        assert 'Hello' in self.app.get('/').data
+        assert '<h1>Hello' in self.app.get('/').data
